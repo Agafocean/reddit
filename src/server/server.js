eb620f1bc53a2d2875154394e55d7251356a0c7e
Evgeny Agafonov <sth179@gmail.com>
2024-02-17 11:55:35 +1000

    reddit_best, v.2

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ app.use('/static', express.static('./dist/client'));
 app.get('/auth', (req, res) => { 
   axios.post(
     'https://www.reddit.com/api/v1/access_token',
-    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=https://skillbox-react-app.onrender.com/auth`,
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=https://reddit-best.onrender.com/auth`,
     {
       auth: { username: process.env.CLIENT_ID, password: 'hpZEsQlTK5IQobZd8rSJy54oL9R7AQ' },
       headers: { 'Content-type': 'application/x-www-form-urlencoded' }
